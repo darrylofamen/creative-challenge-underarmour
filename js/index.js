eae4e85706ade1bc2dba5b29eb40b75408547e2a
Darryl <darryl.ofamen@gmail.com>
2026-02-25 14:39:47 +0800

    create ui panel 1

---
+++ js/index.js
@@ -0,0 +1,5 @@
+// const overlays = document.querySelectorAll("[class^=overlay-]");
+
+// const tl = new TimelineMax();
+
+// tl.staggerTo(overlays, 0.8, { width: 0, ease: Power4.easeInOut }, 0.05);
