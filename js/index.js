a4cf805aab63923d1a8b36649c3a31118568203c
Darryl <darryl.ofamen@gmail.com>
2026-02-26 10:24:36 +0800

    create ui panel 2

--- js/index.js
+++ js/index.js
@@ -3,3 +3,11 @@
 // const tl = new TimelineMax();
 
 // tl.staggerTo(overlays, 0.8, { width: 0, ease: Power4.easeInOut }, 0.05);
+
+const details = document.querySelectorAll("[class^=product-details]");
+const text = new SplitText(details, {
+  type: "lines",
+  linesClass: "product-details-line++",
+});
+
+console.log(text);
