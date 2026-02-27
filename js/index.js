f0d2615fc74a868c90348380e2e37924e2229db5
Darryl <darryl.ofamen@gmail.com>
2026-02-27 16:37:34 +0800

    pre final animation

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
 // Elements
 // Header
 const header = document.querySelector(".header");
-// const headerBrandLogo = document.querySelector(".header #brand-logo");
-// const headerBrandText = document.querySelector(".header #brand-text");
 
 // Logo
 const brandContainer = document.querySelector(".logo");
@@ -28,6 +26,22 @@ const cta = document.querySelector(".cta");
 const endFrame = document.querySelector(".end-frame");
 const endFrameText = document.querySelector(".end-frame-text");
 
+// Playback controls
+const playButton = document.querySelector(".play-button");
+const pauseButton = document.querySelector(".pause-button");
+const reverseButton = document.querySelector(".reverse-button");
+const resumeButton = document.querySelector(".resume-button");
+const restartButton = document.querySelector(".restart-button");
+const playIcon = document.querySelector("#play-icon");
+const pauseIcon = document.querySelector("#pause-icon");
+const reverseIcon = document.querySelector("#reverse-icon");
+const resumeIcon = document.querySelector("#resume-icon");
+const restartIcon = document.querySelector("#restart-icon");
+
+// Progress bar
+const progressBar = document.querySelector(".scrub-bar");
+
+// Timeline
 function createSplitText(element, type, lineClass) {
   return new SplitText(element, {
     type: type,
@@ -37,9 +51,12 @@ function createSplitText(element, type, lineClass) {
 
 function init() {
   const tl = new TimelineMax({
-    // repeat: -1,
-    // repeatDelay: 2,
-    onUpdate: () => console.log(tl.time()),
+    onUpdate: () => {
+      console.log(tl.time());
+      progressBar.step = 0.001;
+      progressBar.value = tl.progress() * 1;
+      progressBar.style.background = `linear-gradient(to right, #000 0%, #000 ${tl.progress() * 100}%, #f0f0f0 ${tl.progress() * 100}%, #f0f0f0 100%)`;
+    },
   });
   const { lines: productDetails1Lines } = createSplitText(
     productDetails1,
@@ -94,7 +111,7 @@ function init() {
       0.6,
       { autoAlpha: 0, yPercent: -150, ease: Power4.easeOut },
       { autoAlpha: 1, yPercent: 0, ease: Power4.easeOut },
-      "=+0.5"
+      "-=0.2"
     )
     // Animate header to slide in
     .add("header-scene")
@@ -112,7 +129,7 @@ function init() {
         immediateRender: false,
       },
       { x: 0, xPercent: 0, left: 10, ease: Power4.easeOut },
-      "header-scene+=0.2"
+      "header-scene-=0.1"
     )
     .fromTo(
       brandText,
@@ -134,7 +151,7 @@ function init() {
         right: 10,
         ease: Power4.easeOut,
       },
-      "header-scene+=0.2"
+      "header-scene-=0.1"
     )
     // Prepare panel 2
     .set(panel2, { top: 40 }, "panel2-scene")
@@ -154,17 +171,17 @@ function init() {
     .staggerFromTo(
       productDetails1Lines,
       0.6,
-      { autoAlpha: 0, y: "+=40" },
-      { autoAlpha: 1, y: 0 },
-      0.2,
+      { autoAlpha: 0, y: "+=50", ease: Power4.easeOut },
+      { autoAlpha: 1, y: 0, ease: Power4.easeOut },
+      0.1,
       "-=0.2"
     )
     // Animate CTA
     .fromTo(
       cta,
-      0.5,
-      { autoAlpha: 0, scale: 0, ease: Back.easeOut.config(2.5) },
-      { autoAlpha: 1, scale: 1, ease: Back.easeOut.config(2.5) },
+      0.4,
+      { autoAlpha: 0, scale: 0, ease: Back.easeOut.config(1.5) },
+      { autoAlpha: 1, scale: 1, ease: Back.easeOut.config(1.5) },
       "-=0.5"
     )
     .add("product-1-leave-scene")
@@ -193,9 +210,9 @@ function init() {
     .staggerFromTo(
       productDetails2Lines,
       0.6,
-      { autoAlpha: 0, y: "+=40" },
-      { autoAlpha: 1, y: 0 },
-      0.2,
+      { autoAlpha: 0, y: "+=50", ease: Power4.easeOut },
+      { autoAlpha: 1, y: 0, ease: Power4.easeOut },
+      0.1,
       "-=0.5"
     )
     // Fade out second product image and product details
@@ -223,9 +240,9 @@ function init() {
     .staggerFromTo(
       productDetails3Lines,
       0.6,
-      { autoAlpha: 0, y: "+=40" },
-      { autoAlpha: 1, y: 0 },
-      0.2,
+      { autoAlpha: 0, y: "+=50", ease: Power4.easeOut },
+      { autoAlpha: 1, y: 0, ease: Power4.easeOut },
+      0.1,
       "-=0.5"
     )
     .add("product-3-leave-scene")
@@ -247,29 +264,142 @@ function init() {
     .to(
       cta,
       0.6,
-      { top: 350, left: 80, ease: Power1.easeOut },
+      { top: 350, left: 80, ease: Power2.easeOut },
       "end-frame-text-scene"
     )
     .fromTo(
       endFrame,
-      0.6,
-      { autoAlpha: 0, ease: Power1.easeOut },
-      { autoAlpha: 1, ease: Power1.easeOut },
+      1,
+      { autoAlpha: 0, ease: Power4.easeOut },
+      { autoAlpha: 1, ease: Power4.easeOut },
       "end-frame-text-scene"
     )
     .staggerFromTo(
       endFrameTextLines,
-      0.6,
-      { autoAlpha: 0, x: -250, ease: Power1.easeOut },
-      { autoAlpha: 1, x: 0, ease: Power1.easeOut },
-      0.6,
-      "end-frame-text-scene"
+      1,
+      { autoAlpha: 0, x: -250, ease: Power4.easeOut },
+      { autoAlpha: 1, x: 0, ease: Power4.easeOut },
+      1,
+      "end-frame-text-scene+=0.1"
     );
 
-  // tl.seek("header-scene");
-  // tl.pause("panel2-scene");
-  // tl.timeScale(1.2);
-  // tl.staggerTo(overlays, 0.8, { width: 0, ease: Power4.easeInOut }, 0.05);
+  // Handlers
+  playButton.addEventListener("click", () => {
+    if (tl.progress() === 1) tl.restart();
+
+    tl.play();
+  });
+  pauseButton.addEventListener("click", () => {
+    tl.pause();
+  });
+  reverseButton.addEventListener("click", () => {
+    tl.reverse();
+  });
+  resumeButton.addEventListener("click", () => {
+    tl.resume();
+  });
+  restartButton.addEventListener("click", () => {
+    tl.restart();
+  });
+
+  // This function will run continuously as the thumb is dragged
+  progressBar.addEventListener("input", function (e) {
+    const value = e.target.value;
+    this.style.background = `linear-gradient(to right, #000 0%, #000 ${value * 100}%, #f0f0f0 ${value * 100}%, #f0f0f0 100%)`;
+
+    // tl.pause();
+    tl.progress(value);
+  });
+
+  playButton.addEventListener("mouseenter", () => {
+    TweenLite.set(playIcon, {
+      fill: "#fff",
+    });
+    TweenLite.set(playButton, {
+      backgroundColor: "#000",
+    });
+  });
+
+  playButton.addEventListener("mouseleave", () => {
+    TweenLite.set(playIcon, {
+      fill: "#000",
+    });
+    TweenLite.set(playButton, {
+      backgroundColor: "#fff",
+    });
+  });
+
+  pauseButton.addEventListener("mouseenter", () => {
+    TweenLite.set(pauseIcon, {
+      fill: "#fff",
+    });
+    TweenLite.set(pauseButton, {
+      backgroundColor: "#000",
+    });
+  });
+
+  pauseButton.addEventListener("mouseleave", () => {
+    TweenLite.set(pauseIcon, {
+      fill: "#000",
+    });
+    TweenLite.set(pauseButton, {
+      backgroundColor: "#fff",
+    });
+  });
+
+  reverseButton.addEventListener("mouseenter", () => {
+    TweenLite.set(reverseIcon, {
+      fill: "#fff",
+    });
+    TweenLite.set(reverseButton, {
+      backgroundColor: "#000",
+    });
+  });
+
+  reverseButton.addEventListener("mouseleave", () => {
+    TweenLite.set(reverseIcon, {
+      fill: "#000",
+    });
+    TweenLite.set(reverseButton, {
+      backgroundColor: "#fff",
+    });
+  });
+
+  resumeButton.addEventListener("mouseenter", () => {
+    TweenLite.set(resumeIcon, {
+      fill: "#fff",
+    });
+    TweenLite.set(resumeButton, {
+      backgroundColor: "#000",
+    });
+  });
+
+  resumeButton.addEventListener("mouseleave", () => {
+    TweenLite.set(resumeIcon, {
+      fill: "#000",
+    });
+    TweenLite.set(resumeButton, {
+      backgroundColor: "#fff",
+    });
+  });
+
+  restartButton.addEventListener("mouseenter", () => {
+    TweenLite.set(restartIcon, {
+      fill: "#fff",
+    });
+    TweenLite.set(restartButton, {
+      backgroundColor: "#000",
+    });
+  });
+
+  restartButton.addEventListener("mouseleave", () => {
+    TweenLite.set(restartIcon, {
+      fill: "#000",
+    });
+    TweenLite.set(restartButton, {
+      backgroundColor: "#fff",
+    });
+  });
 }
 
 window.addEventListener("load", init);
